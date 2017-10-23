eeff4f062b90c7089feb0e60cbc36a2bfeb4525a
Denys Astapov <cr0we@icloud.com>
2017-10-23 22:56:12 +0300

    Initial Commit

---
+++ BoolsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoolsEye
+//
+//  Created by Denys Astapov on 23.10.2017.
+//  Copyright © 2017 Denys Astapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
